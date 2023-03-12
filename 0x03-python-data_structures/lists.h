0fe85597ab42d53236d2887b2794f420f51f4fb0
nebihadilsebo <shegolenur@hotmail.com>
2023-03-12 12:35:47 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def print_list_integer(my_list=[])
+	def element_at(my_list, idx)
+	def replace_in_list(my_list, idx, element)
+	def print_reversed_list_integer(my_list=[])
+	def new_in_list(my_list, idx, element)
+	def no_c(my_string)
+
+#endif
