d7185340f83a9e07c0dd68333b6eb29520a3fcb5
nebihadilsebo <shegolenur@hotmail.com>
2023-03-13 10:19:30 -0700

    python-data_structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    updated_str = ''
+    for i in range(my_string):
+        if i != 'c' or i != 'C':
+            updated_str += i
+        return(update_str)
+
