98dd642082f8bfe8db30e36e41734a4d71d57648
nebihadilsebo <shegolenur@hotmail.com>
2023-03-27 06:40:05 -0700

    python and exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     total = 0
-    for i in range(x):
-        try:
-            print("{my_list[i]}", end="")
+    try:
+        for i in range (x):
+            print(my_list[i], end="")
             total += 1
-        except:
-            break
+    except:
+        pass
+    finally:
         print()
-        return(total)    
+    return(total)    
