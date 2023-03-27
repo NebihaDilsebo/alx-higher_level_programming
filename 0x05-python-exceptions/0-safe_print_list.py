556d7504dc5e0cc6b07d59a8859c362d57857676
nebihadilsebo <shegolenur@hotmail.com>
2023-03-27 07:58:16 -0700

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             total += 1
-    except Index Error:
+    except IndexError:
         pass
     print()
     return(total)
