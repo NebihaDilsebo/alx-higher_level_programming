18ca388fedaa7e669b5f3fb5bed82e1fa798eabe
nebihadilsebo <shegolenur@hotmail.com>
2023-03-28 08:02:16 -0700

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 # 0-square.py by Nebiha Dilsebo
-"""a module that defines a square """
+"""A module that defines a square """
 
 
 class Square:
     """A class that represents a square"""
 
     def __init__(self, size=0):
-        """initializing this square class
+        """Initializing this square class
         Args:
-            size: represents the size of the square defined
+            size: represnets the size of the square defined
         Raises:
-            TypeError: if size is not intiger
+            TypeError: if size is not integer
             ValueError: if size is less than zero
         """
 
         if not isinstance(size, int):
-            raise TypeError('size must be an intiger')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
 
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# 0-square.py by Nebiha Dilsebo
+"""a module that defines a square """
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not intiger
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an intiger')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+        def are(self):
+            """
+            calculate area of the square
+            Returns: The square os the size
+            """
+
+            return(self.__size**2)
