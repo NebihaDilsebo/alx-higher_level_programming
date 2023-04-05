c50b3a8ad8d00234f6a91b749f09c532a0ed2da5
nebihadilsebo <shegolenur@hotmail.com>
2023-04-05 00:14:03 -0700

    python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,17 @@
+#!/usr/bin/python3
+# 0-add_intiger by Nebiha Dilsebo
+
 def add_integer(a, b=98):
     """ checks if  and b are intiger or float"""
     if not isinstance (a, (int, float)):
-        raise TypeError ("a must be an integer or b must be an integer")
+        raise TypeError ("a must be an integer")
     if not isinstance (b, (int, float)):
-        raise TypeError ("a must be an integer or b must be an integer")
+        raise TypeError ("b must be an integer")
     """casts a and b in to int if they are float"""
     if isinstance (a, float):
+        a = int(a)
+        b = int(b)
+    if isinstance (b, float):
         a = int(a)
         b = int(b)
     """return the sum of intigers"""
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,8 @@
 def matrix_divided(matrix, div):
-    """checks if matrix are intigers or float"""
-    if not isinstance (matrix, (int,float)):
+    """checks if matrix is a list of lists of integer or floats"""
+    if not all (isinstance (row, list) and all(isinstance(element, (int, float)) for element in row) for row in matrix):
         raise TypeError ("matrix must be a matrix (list of lists) of integers/floats")
+    """checks if all rows have the same size
     if not all(len(lists) == len(matrix[0]) for lists in matrix):
         raise TypeError ("Each row of the matrix must have the same size")
     if not isinstance (div, (int,float)):
