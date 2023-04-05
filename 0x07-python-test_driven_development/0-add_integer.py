2f54df9de6cab31a5ebf48b222ddc811e0d084ce
nebihadilsebo <shegolenur@hotmail.com>
2023-04-05 00:58:08 -0700

    python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 # 0-add_intiger by Nebiha Dilsebo
 
+"""this module adds intiger"""
+
+
 def add_integer(a, b=98):
     """ checks if  and b are intiger or float"""
     if not isinstance (a, (int, float)):
         raise TypeError ("a must be an integer")
     if not isinstance (b, (int, float)):
         raise TypeError ("b must be an integer")
-    """casts a and b in to int if they are float"""
-    if isinstance (a, float):
-        a = int(a)
-        b = int(b)
-    if isinstance (b, float):
-        a = int(a)
-        b = int(b)
+    a = int(a)
+    b = int(b)
     """return the sum of intigers"""
     return (a + b)
