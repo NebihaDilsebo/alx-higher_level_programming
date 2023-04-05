ae318162c87a8178ecd881e9f2c2a16dacb45a6f
nebihadilsebo <shegolenur@hotmail.com>
2023-04-05 08:41:47 -0700

    python-more_classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,12 @@
 
 class Rectangle:
     """represents a rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """contruct a function using property and setter"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
