9848fe7b71d7b9deab8fb4485fbc66928ba7b173
nebihadilsebo <shegolenur@hotmail.com>
2023-04-05 01:02:34 -0700

    python-test_driven_development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,10 +6,10 @@
 
 def add_integer(a, b=98):
     """ checks if  and b are intiger or float"""
-    if not isinstance (a, (int, float)):
-        raise TypeError ("a must be an integer")
-    if not isinstance (b, (int, float)):
-        raise TypeError ("b must be an integer")
+    if not(isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    if not(isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
     """return the sum of intigers"""
