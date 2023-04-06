58fa0e865fe67097eb827b2eef2aaa9debe2904d
nebihadilsebo <shegolenur@hotmail.com>
2023-04-05 18:12:29 -0700

    python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""defines a locked class"""
+
+
 class LockedClass:
     __slots__ = ["first_name"]
+    """sets an attribute"""
 
     def __setattr__(self, key, value):
         if not hasattr(self, key) and key != "first_name":
