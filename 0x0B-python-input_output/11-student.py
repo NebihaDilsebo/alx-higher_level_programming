9c1b8e234a97557498e7578214d91c224bccb049
nebihadilsebo <shegolenur@hotmail.com>
2023-04-11 14:11:52 -0700

    python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+class Student:
+    """represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes a new student
+        Args:
+        first_name(str) = the students first name
+        last_name(str) = the students last name
+        age(int) = the student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all the student attribute.
+        Args:
+            json (dict): pair to replace the attribute
+            """
+        for k, v in json.items()
+        setattr(self, k, v)
