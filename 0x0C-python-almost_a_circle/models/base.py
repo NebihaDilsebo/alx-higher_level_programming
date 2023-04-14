a6f2de98c57eb3080f1a10d66ece46dc969beb5b
nebihadilsebo <shegolenur@hotmail.com>
2023-04-14 11:22:48 -0700

    python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"represents a class base"""
+
+
+class Base:
+    """creates a private  class attribute base"""
+
+    __nb_objects = 0
+    """initializes id"""
+
+
+    def __init__(self, id=None):
+        self.id = id
+        if id !=None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
