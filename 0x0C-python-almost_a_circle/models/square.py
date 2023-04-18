3c128669981b5c12099d7a05b2f3626a997b383e
nebihadilsebo <shegolenur@hotmail.com>
2023-04-18 10:28:47 -0700

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """module that inherits from Base"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """square class that inherits from Rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """initializes an instance of Square class"""
         super().__init__(size, size, x, y, id)
@@ -17,21 +19,22 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.heigt = value
+
     def __str__(self):
         """updates by overriding"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}\
+                .format self.id, self.x, self.y, self.width, self.height"
 
     def update(self, *args, **kwargs):
-            """method that updats the attribute of a rectangle """
-            if args:
-                attrs = ["id", "width", "height", "x", "y"]
-                for idx, value in enumerate(args):
-                    setattr(self, attrs[idx], value)
+        """method that updats the attribute of a rectangle """
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for idx, value in enumerate(args):
+                setattr(self, attrs[idx], value)
             elif kwargs:
                 for key, value in kwargs.items():
                     setattr(self, key, value)
 
     def to_dictionary(self):
-            """ method that returns the dictionary representation of a rectangle"""
-            return {'id': self.id, 'size':self.width, 'x': self.x, 'y':self.y}
-       
+        """returns the dictionary representation of a rectangle"""
+    return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
