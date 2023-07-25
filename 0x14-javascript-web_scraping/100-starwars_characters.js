c3943bd4fd451107f26fe6d5a27ec65a97b59819
NebihaDilsebo <shegolenur@hotmail.com>
2023-07-25 10:27:02 -0700

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+#!usr/bin/node
+
+const request = require('request');
+
+function getCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const characters = movieData.characters;
+
+      characters.forEach(function (characterUrl) {
+        request(characterUrl, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+          } else {
+            console.log('Error fetching character data:', error);
+          }
+        });
+      });
+    } else {
+      console.log('Error fetching movie data:', error);
+    }
+  });
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Please provide a movie ID as the first argument.');
+} else {
+  getCharacters(movieId);
+}
