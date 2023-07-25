41bc386c72d39ec807c6307cdda177c68f75ee17
NebihaDilsebo <shegolenur@hotmail.com>
2023-07-25 10:57:43 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,33 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
+let data;
+const dictionary = {};
 
-function countCompletedTasks(apiUrl) {
-  request(apiUrl, function (error, response, body) {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          if (completedTasksByUser[todo.userId]) {
-            completedTasksByUser[todo.userId]++;
-          } else {
-            completedTasksByUser[todo.userId] = 1;
-          }
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
         }
-      });
-
-      console.log(completedTasksByUser);
-    }
-  });
-}
-
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 3) {
-  console.error('Usage: node completed_tasks.js <API_URL>');
-} else {
-  const apiUrl = process.argv[2];
-  countCompletedTasks(apiUrl);
-}
+        dictionary[userid] += 1;
+      }
+    });
+    console.log(dictionary);
+  }
+});
